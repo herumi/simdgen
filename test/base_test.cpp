da0193be47a18f6406519fb7f1d4cd1dd0309e85
MITSUNARI Shigeo <herumi@nifty.com>
2021-08-27 12:31:51 +0900

    [test] base_test uses cybozu/test

--- test/base_test.cpp
+++ test/base_test.cpp
@@ -1,16 +1,25 @@
+#define CYBOZU_TEST_DISABLE_AUTO_RUN
+#include <cybozu/test.hpp>
 #include "parser.hpp"
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+const char *g_src;
+
+CYBOZU_TEST_AUTO(print)
 {
-	const char *src = argc == 1 ? "x" : argv[1];
-	printf("src=%s\n", src);
+	printf("src=%s\n", g_src);
 	sg::TokenList tl;
 	tl.setVar("x");
 	sg::Parser parser;
-	parser.parse(tl, src);
+	parser.parse(tl, g_src);
 	tl.put();
 	sg::GeneratorBase gen;
 	gen.debug = true;
 	gen.exec(tl);
+}
+
+int main(int argc, char *argv[])
+{
+	g_src = argc == 1 ? "x" : argv[1];
+	return cybozu::test::autoRun.run(argc, argv);
 }
