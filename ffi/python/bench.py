627180a07f0079081c86842bdb4d6cbf95626573
MITSUNARI Shigeo <herumi@nifty.com>
2021-11-19 16:54:26 +0900

    [python] add bench

---
+++ ffi/python/bench.py
@@ -0,0 +1,31 @@
+import simdgen
+import numpy
+import timeit
+import math
+
+N = 8192
+C = 1000
+a = numpy.arange(N, dtype='float32')
+b = numpy.arange(N, dtype='float32')
+
+for i in range(N):
+	a[i] = math.sin(i * 0.1) * 3
+
+src = "log(cosh(x))"
+sg1 = simdgen.SgCode(src)
+print("func", src)
+print("simdgen", timeit.timeit(lambda: sg1.calc(b, a), number=C))
+print("numpy  ", timeit.timeit(lambda: numpy.log(numpy.cosh(a)), number=C))
+
+print("result top")
+c = numpy.log(numpy.cosh(a))
+for i in range(10):
+	print(b[i], c[i], math.fabs(b[i] - c[i]))
+
+src = "red_sum(log(cosh(x)))"
+print("func", src)
+sg2 = simdgen.SgCode(src)
+print("simdgen", timeit.timeit(lambda: sg2.calc(a), number=C))
+print("numpy  ", timeit.timeit(lambda: numpy.sum(numpy.log(numpy.cosh(a))), number=C))
+
+print("result", sg2.calc(a), numpy.sum(numpy.log(numpy.cosh(a))))
--- ffi/python/numpy-sample.py
+++ ffi/python/numpy-sample.py
@@ -1,6 +1,5 @@
 import simdgen
 import numpy
-import ctypes
 
 a = numpy.array([1,2,3,4,5,6,7], dtype='float32')
 b = numpy.arange(len(a), dtype='float32')
