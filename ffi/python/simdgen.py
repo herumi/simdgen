9e97431b908a12738ab4da19e9e2fa60a94ed2c0
MITSUNARI Shigeo <herumi@nifty.com>
2021-11-19 12:39:21 +0900

    update sample

--- ffi/python/simdgen.py
+++ ffi/python/simdgen.py
@@ -20,57 +20,67 @@ def init():
 	g_lib = cdll.LoadLibrary(libName)
 
 class SgCode(Structure):
-	def __init__(self):
-		self.p = 0
-		self.addr = 0
-	def init(self):
-		if not self.p:
-			if not g_lib:
-				init()
-			self.p = c_void_p(g_lib.SgCreate())
-			if self.p == 0:
-				raise RuntimeError("SgCreate")
-			print(f"self.p={self.p}")
-	def destroy(self):
-		g_lib.SgDestroy(self.p)
-		self.p = 0
-	def getFuncAddr(self, src, varName):
-		if self.addr:
-			raise RuntimeError("already getFunc")
-		g_lib.SgGetFuncAddr.restype = CFUNCTYPE(c_void_p, POINTER(c_float), POINTER(c_float), c_size_t)
+	def __init__(self, src, varName="x"):
+		if not g_lib:
+			init()
+		self.p = c_void_p(g_lib.SgCreate())
+		if self.p == 0:
+			raise RuntimeError("SgCreate")
+		self.reduce = src.find("red_sum") >= 0
+		if self.reduce:
+			g_lib.SgGetFuncAddr.restype = CFUNCTYPE(c_float, POINTER(c_float), c_size_t)
+		else:
+			g_lib.SgGetFuncAddr.restype = CFUNCTYPE(c_void_p, POINTER(c_float), POINTER(c_float), c_size_t)
 		self.addr = g_lib.SgGetFuncAddr(self.p, c_char_p(src.encode()), c_char_p(varName.encode()))
 		if not self.addr:
-			raise RuntimeError("bad param", varName, src)
-		return self.addr
-	def calc(self, dst, src):
-		self.addr(cast(byref(dst), POINTER(c_float)), cast(byref(src), POINTER(c_float)), len(src))
+			raise RuntimeError("bad param", src, varName)
+	def destroy(self):
+		if g_lib:
+			g_lib.SgDestroy(self.p)
+		self.p = 0
+	def calc(self, p1, p2=None):
+		if self.reduce:
+			return self.addr(cast(byref(p1), POINTER(c_float)), len(p1))
+		else:
+			if len(p1) != len(p2):
+				raise RuntimeError("bad length", len(p1), len(p2))
+			self.addr(cast(byref(p1), POINTER(c_float)), cast(byref(p2), POINTER(c_float)), len(p1))
+			return 0
 
 if __name__ == '__main__':
 	print("len=", len(sys.argv))
 	if len(sys.argv) > 1:
 		funcStr = sys.argv[1]
 	else:
-		funcStr = "log(cosh(x))+100"
+		funcStr = "exp(x*x*0.1+0.3)"
 	print(f"funcStr={funcStr}")
-	sg = SgCode()
-	sg.init()
-	sg.getFuncAddr(funcStr, "x")
+	sg = SgCode(funcStr)
 
-	dst = (c_float * 16)()
-	src = (c_float * 16)()
-	for i in range(len(dst)):
-		src[i] = i * 0.1
-	sg.calc(dst, src)
-	print("dst")
 	import math
-	for i in range(len(dst)):
-		a = dst[i]
-		if funcStr == "log(cosh(x))+100":
-			b = math.log(math.cosh(src[i]))+100
-			print(a, b, math.fabs(a - b))
-		else:
-			print(a)
-
+	def f(x):
+		return eval(funcStr.replace("red_sum", ""), {"x":x, "exp":math.exp, "log":math.log, "cosh":math.cosh})
+	N = 1024
+	if sg.reduce:
+		a = 0
+		src = (c_float * N)()
+		for i in range(N):
+			src[i] = math.sin(i) * 2.4
+			a = a + f(src[i])
+		b = sg.calc(src)
+		print('ok', a)
+		print('my', b)
+	else:
+		dst = (c_float * N)()
+		src = (c_float * N)()
+		for i in range(N):
+			src[i] = i * 0.1
+		sg.calc(dst, src)
+		for i in range(min(N, 16)):
+			a = f(src[i])
+			b = dst[i]
+			if math.fabs(a - b) > 1e-5:
+				print(f"err a={a} b={b}")
+	print("ok")
 	sg.destroy()
 
 
